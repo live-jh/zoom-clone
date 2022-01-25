71f2ca4f1879d54659a3ff651a086323768b7da2
live-jh <livejh7129@gmail.com>
2022-01-25 18:24:14 +0900

    finish done -> socket io 시작

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import WebSocket from "ws";
-import {onSocketClose, onSocketMessage} from "./common"
+import {onSocketMessage} from "./common"
 
 const app = express();
 app.set("view engine", "pug");
@@ -15,14 +15,14 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 // http 서버 위에 ws 서버올려서 함께 돌리기 (express는 ws를 지원 x)
 // http 서버가 필요한 이유는 views, static fiels, home, redirect를 지원해야하기 때문
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
 
 const sockets = []; // 빈 리스트
+const wss = new WebSocket.Server({server});
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
     socket["nickname"] = "anonymous";
-    console.log(socket)
+    console.log(socket);
     console.log("connected to Browser ✔");
     socket.on("close", () => (console.log("disconnected to Browser 𝘟")));
     socket.on("message", (message) => onSocketMessage(socket, sockets, message));
